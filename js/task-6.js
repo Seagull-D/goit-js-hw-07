ea75f996f5d0c9d12f6f3be1aab6c8d329e9bc1c
Seagull-D <djseagull90@gmail.com>
2024-11-02 11:03:34 +0100

    2.11
    task 6 edit

--- js/task-6.js
+++ js/task-6.js
@@ -3,43 +3,45 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
 const inputCreate = document.querySelector("#controls input[type='number']");
 const createBtn = document.querySelector("#controls button[data-create]");
 const destroyBtn = document.querySelector("#controls button[data-destroy]");
 const allBoxes = document.querySelector("#boxes");
-
-inputCreate.addEventListener("input", enterToInput);
+inputCreate.parentNode.classList.add("controls");
 createBtn.addEventListener("click", createElem);
 destroyBtn.addEventListener("click", destroyElem);
-console.dir(inputCreate);
-const boxes = [];
 
-function enterToInput(evt) {
-  console.dir(evt.currentTarget);
-  const value = Number(evt.currentTarget.valueAsNumber);
-  if (value >= 1 && value <= 100) {
-    console.log(value);
-    let boxSize = 30;
-    for (let i = 0; i < value; i++) {
-      const div = document.createElement("div");
-      div.style.width = `${boxSize}px`;
-      div.style.height = `${boxSize}px`;
-      div.style.backgroundColor = getRandomHexColor();
-      boxSize += 10;
-      boxes.push(div);
-    }
+function createBoxes(amount) {
+  const boxes = [];
+  let boxSize = 30;
+
+  for (let i = 0; i < amount; i++) {
+    const div = document.createElement("div");
+    div.style.width = `${boxSize}px`;
+    div.style.height = `${boxSize}px`;
+    div.style.backgroundColor = getRandomHexColor();
+    boxSize += 10;
+    boxes.push(div);
+  }
+  allBoxes.innerHTML = "";
+  allBoxes.append(...boxes);
+}
+function createElem() {
+  const inputNumb = Number(inputCreate.value);
+
+  if (inputNumb >= 1 && inputNumb <= 100) {
+    createBoxes(inputNumb);
+    inputCreate.value = "";
+    inputCreate.style.borderColor = "";
   } else {
     inputCreate.style.borderColor = "red";
-    allBoxes.innerHTML = "";
   }
-}
-console.log(boxes);
-function createElem() {
-  allBoxes.append(...boxes);
-  inputCre.value = "";
+  console.log(inputNumb);
 }
 
 function destroyElem() {
   allBoxes.innerHTML = "";
+  inputCreate.value = "";
+  inputCreate.style.borderColor = "";
 }
-console.log(boxes);
